a9762e4c4db51eed1252aa0c349202af6acd2ca7
shakeelanjum <shakeelmanjum@gmail.com>
2014-04-22 19:40:24 +0500

    A. Net wrapper for Evercam API

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -12,20 +12,20 @@ namespace Test
         static void Main(string[] args)
         {
             Evercam evercam = new Evercam("api_id", "api_key");
-            CameraInfo info = new CameraInfo() { 
-                ID = "test",
-                Name = "Test Camera",
-                Username = "user",
-                Password = "12345",
-                IsPublic = false,
-                ExternalHost = "100.100.100.100",
-                InternalHost = "192.168.1.100",
-                ExternalHttpPort = "8080",
-                InternalHttpPort = "80",
-                JpegUrl = "snapshots/image.jpg"
-            };
-            Camera camera = evercam.CreateCamera(info);
-
+            //CameraInfo info = new CameraInfo() { 
+            //    ID = "test",
+            //    Name = "Test Camera",
+            //    Username = "user",
+            //    Password = "12345",
+            //    IsPublic = false,
+            //    ExternalHost = "100.100.100.100",
+            //    InternalHost = "192.168.1.100",
+            //    ExternalHttpPort = "8080",
+            //    InternalHttpPort = "80",
+            //    JpegUrl = "snapshots/image.jpg"
+            //};
+            //Camera camera = evercam.CreateCamera(info);
+            var live = evercam.GetLiveImage("test");
             var cams = evercam.GetCameras("joeyb");
         }
     }
